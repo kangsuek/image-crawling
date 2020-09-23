44cac4f01e7ba72e6a201fa2e15eb34e58245e8a
김강석 <kangsuek@gimgangseog-ui-MacBookPro.local>
2020-09-23 11:34:07 +0900

    수정

--- run_crawling.sh
+++ run_crawling.sh
@@ -1,1 +1,3 @@
 python3 main.py -u https://www.g9.co.kr/Display/Category/400000076/0/0 -c clothes -s 1 -e 10
+
+python3 main.py -u https://www.g9.co.kr/Display/Category/400000076/500000368 -c women_tops -s 1 -e 10
